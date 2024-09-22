1a6b499e55ed6435922186b5659de24847423f58
saamirkc <samir.kc.a20@gmail.com>
2024-09-22 10:49:21 +0545

    Add generated static files

---
+++ simply-static-1-1726980346/wp-content/uploads/uag-plugin/assets/1000/uag-js-1201.js
@@ -0,0 +1,84 @@
+document.addEventListener("DOMContentLoaded", function(){ 				window.addEventListener( 'DOMContentLoaded', () => {
+					const blockScope = document.querySelector( '.uagb-block-1c0f4dcb' );
+					if ( ! blockScope ) {
+						return;
+					}
+
+					const anchorElement = blockScope.querySelector('a');
+					if (!anchorElement) {
+						return;
+					} 
+
+					 
+					blockScope.addEventListener('keydown', (event) => {
+						if ( 13 === event.keyCode || 32 === event.keyCode ) {
+							event.preventDefault();
+							 
+							anchorElement.click();	
+						}
+					} );
+				} );
+							window.addEventListener( 'DOMContentLoaded', () => {
+					const blockScope = document.querySelector( '.uagb-block-60c5f9a2' );
+					if ( ! blockScope ) {
+						return;
+					}
+
+					const anchorElement = blockScope.querySelector('a');
+					if (!anchorElement) {
+						return;
+					} 
+
+					 
+					blockScope.addEventListener('keydown', (event) => {
+						if ( 13 === event.keyCode || 32 === event.keyCode ) {
+							event.preventDefault();
+							 
+							anchorElement.click();	
+						}
+					} );
+				} );
+							window.addEventListener( 'DOMContentLoaded', () => {
+					const blockScope = document.querySelector( '.uagb-block-21687caf' );
+					if ( ! blockScope ) {
+						return;
+					}
+
+					const anchorElement = blockScope.querySelector('a');
+					if (!anchorElement) {
+						return;
+					} 
+
+					 
+					blockScope.addEventListener('keydown', (event) => {
+						if ( 13 === event.keyCode || 32 === event.keyCode ) {
+							event.preventDefault();
+							 
+							anchorElement.click();	
+						}
+					} );
+				} );
+							window.addEventListener( 'DOMContentLoaded', () => {
+					const blockScope = document.querySelector( '.uagb-block-25b3fcde' );
+					if ( ! blockScope ) {
+						return;
+					}
+
+					const anchorElement = blockScope.querySelector('a');
+					if (!anchorElement) {
+						return;
+					} 
+
+					 
+					blockScope.addEventListener('keydown', (event) => {
+						if ( 13 === event.keyCode || 32 === event.keyCode ) {
+							event.preventDefault();
+							 
+							anchorElement.click();	
+						}
+					} );
+				} );
+			window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"18b3c5c9","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"samir.kc.a20@gmail.com","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-18b3c5c9', 1201 );
+});
+ });
---
+++ simply-static-1-1726980346/wp-content/uploads/uag-plugin/assets/1000/uag-js-1203.js
@@ -0,0 +1,85 @@
+document.addEventListener("DOMContentLoaded", function(){ var ssLinksParent = document.querySelector( '.uagb-block-71947d21' );
+ssLinksParent?.addEventListener( 'keyup', function ( e ) {
+var link = e.target.closest( '.uagb-ss__link' );
+if ( link && e.keyCode === 13 ) {
+	handleSocialLinkClick( link );
+}
+});
+
+ssLinksParent?.addEventListener( 'click', function ( e ) {
+var link = e.target.closest( '.uagb-ss__link' );
+if ( link ) {
+	handleSocialLinkClick( link );
+}
+});
+
+function handleSocialLinkClick( link ) {
+var social_url = link.dataset.href;
+var target = "";
+if ( social_url == "mailto:?body=" ) {
+	target = "_self";
+}
+var request_url = "";
+if ( social_url.indexOf("/pin/create/link/?url=") !== -1 ) {
+	request_url = social_url + encodeURIComponent( window.location.href ) + "&media=" + '';
+} else {
+	request_url = social_url + encodeURIComponent( window.location.href );
+}
+window.open( request_url, target );
+}
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-332364cc', {"layout":"bars","heading":"Javascript","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":75,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-10a0bec4', {"layout":"bars","heading":"HTML","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":67,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-97a5b8ce', {"layout":"bars","heading":"CSS","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":82,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-06b617b6', {"layout":"bars","heading":"Photoshop","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":93,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+				window.addEventListener( 'DOMContentLoaded', () => {
+					const blockScope = document.querySelector( '.uagb-block-ed8b5234' );
+					if ( ! blockScope ) {
+						return;
+					}
+
+					const anchorElement = blockScope.querySelector('a');
+					if (!anchorElement) {
+						return;
+					} 
+
+					 
+					blockScope.addEventListener('keydown', (event) => {
+						if ( 13 === event.keyCode || 32 === event.keyCode ) {
+							event.preventDefault();
+							 
+							anchorElement.click();	
+						}
+					} );
+				} );
+							window.addEventListener( 'DOMContentLoaded', () => {
+					const blockScope = document.querySelector( '.uagb-block-b6378c2a' );
+					if ( ! blockScope ) {
+						return;
+					}
+
+					const anchorElement = blockScope.querySelector('a');
+					if (!anchorElement) {
+						return;
+					} 
+
+					 
+					blockScope.addEventListener('keydown', (event) => {
+						if ( 13 === event.keyCode || 32 === event.keyCode ) {
+							event.preventDefault();
+							 
+							anchorElement.click();	
+						}
+					} );
+				} );
+			window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"18b3c5c9","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"samir.kc.a20@gmail.com","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-18b3c5c9', 1203 );
+});
+ });
